0db094774490ec38471abd72f20bb210f9fd9a13
developer <akrem.abayed@gmail.com>
2015-06-03 10:35:52 +0200

    integrating handlebars plugin

--- examples/templates/handlebarsBrowserify.js
+++
@@ -1,9 +0,0 @@
-var Handlebars = require("handlebars");
-var source = require("./template.hbs");
-
-var template = Handlebars.compile(source);
-var html = template({ name: "Epeli" });
-
-document.write(html);
-//console.log(html);
-
