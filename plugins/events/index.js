d9635f976215d45cc6da338782c50e1300384200
Toni Meuschke <toni.meuschke@googlemail.com>
2015-07-31 11:32:10 +0200

    Fix(plugins): event plugin mediator fix
    - fix the mediator that is on the magga currently and not in defaults

--- plugins/events/index.js
+++ plugins/events/index.js
@@ -1,17 +1,24 @@
-
+/**
+ * TODO rework that to nicer implementaion because Magga.Mediator could be placed on a other object as well like
+ *
+ * Magga.publish and Magga.subscribe
+ * In this case this fn will not work
+ *
+ */
+var Magga = require("magga").getInstance();
 module.exports = {
     beforeInit: function (jig) {
     	var keys,
     		len, i;
-       if(jig.defaults.events){
+       if(jig.defaults.events && Magga.Mediator){
        		keys = Object.keys(jig.defaults.events);
        		for(i = 0, len = keys.length; i < len; i++){
        			if(typeof jig[jig.defaults.events[keys[i]]] === "function"){
 					(function(event){
 						// todo should use bind to keep scope
-		       			jig.defaults.Magga.Mediator.subscribe(event, function(){
+		       			Magga.Mediator.subscribe(event, function(){
 		       				jig[jig.defaults.events[event]].apply(jig, arguments);
-		       			}) 
+		       			})
 
 					})(keys[i]);
        			}
